e53a5757c1d5bf47f830f0b71e1d5969bd2f060d
pradumn26 <pradumn.26@gmail.com>
2018-08-13 18:45:43 +0530

    production push 2

---
+++ util/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://pradumn.26:spearbyedge1@ds119802.mlab.com:19802/turnip-test',
+    linkedInClientID: '81wqjbe1fmljvf',
+    linkedInClientSecret: 'ltQYvbqZSWm9p9e2'
+};
