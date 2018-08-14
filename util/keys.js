3868f0e196bb0556e42882eb3d35a84f0c7d741a
pradumn26 <pradumn.26@gmail.com>
2018-08-14 20:51:10 +0530

    do not display keys.js on master branch

--- util/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb://pradumn.26:spearbyedge1@ds119802.mlab.com:19802/turnip-test',
-    linkedInClientID: '81wqjbe1fmljvf',
-    linkedInClientSecret: 'ltQYvbqZSWm9p9e2'
-};
